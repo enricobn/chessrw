be649b6c627e654e5f3e8c5a30c532e16670ea46
Enrico Benedetti <enrico_benedetti@yahoo.com>
2018-08-20 04:26:47 +0200

    Add arguments to bin.

--- src/bin/chessparser.rs
+++ src/bin/chessparser.rs
@@ -1,31 +1,92 @@
 extern crate chessparser;
+extern crate clap;
 
 use std::fs::File;
-use std::env;
 use chessparser::base::parser::*;
+use chessparser::base::writer::*;
 use std::time::Instant;
+use clap::{Arg, App};
+use std::collections::HashMap;
 
 /**
  * ficsgamesdb_201801_standard_nomovetimes_14117.pgn
  * secs: 1, nanos: 964518173
  */
 pub fn main() {
-    let args: Vec<String> = env::args().collect();
-        // let fun = |tags: &HashMap<String,String>| tags.get("Result").map_or_else(|| false, |r| *r != "1/2-1/2".to_string() && 
-    // *r != "0-1".to_string() && *r != "1-0".to_string());
-    //let fun = |tags: &HashMap<String,String>| tags.get("Result").map_or_else(|| false, |r| *r == "0-0".to_string());
-    //let fun = |tags: &HashMap<String,String>| tags.get("Result").is_none();
-    println!("Reding file {} ...", &args[1]);
+
+    let matches = 
+        App::new("Chess read / write")
+            .version("0.1.0")
+            .arg(Arg::with_name("INPUT")
+                .help("Sets the input file to use")
+                .required(true)
+                .index(1))
+            .arg(Arg::with_name("OUTPUT")
+                .help("Sets the output file to use")
+                .required(false)
+                .index(2))
+            .arg(Arg::with_name("nocomments").long("nocomments"))
+            .arg(Arg::with_name("novariations").long("novariations"))
+            .arg(Arg::with_name("whitewins").long("whitewins"))
+            .arg(Arg::with_name("blackwins").long("blackwins"))
+            .arg(Arg::with_name("draw").long("draw"))
+            .get_matches();
+
+    let input = matches.value_of("INPUT").unwrap();
+    println!("Reding file {} ...", &input);
 
     let mut builder = ChessParserBuilder::new();
-    // builder.ignore_comments();
-    // builder.ignore_variations();
+    if matches.is_present("nocomments") {
+        builder.ignore_comments();
+    }
+
+    if matches.is_present("novariations") {
+        builder.ignore_variations();
+    }
+
+    if matches.is_present("whitewins") {
+        builder.tag_filter(&::white_wins);
+    }
+
+    if matches.is_present("blackwins") {
+        builder.tag_filter(&::black_wins);
+    }
+
+    if matches.is_present("draw") {
+        builder.tag_filter(&::draw);
+    }
+
     //builder.tag_filter(&fun);
     let p = builder.build();
 
-    //let file = File::open("/home/enrico/Documents/PGN/ficsgamesdb_201801_standard_nomovetimes_14117.pgn");
-    let file = File::open(&args[1]);
+    let file = File::open(&input);
 
     let start = Instant::now();
-    println!("{} games red in {:?}.", p.parse(file.unwrap()).count(), start.elapsed());
+    if matches.is_present("OUTPUT") {
+        let file_to_write = File::create(matches.value_of("OUTPUT").unwrap());
+        let chess_writer_builder = ChessWriterBuilder{};
+
+        let mut chess_writer = chess_writer_builder.build(file_to_write.unwrap());
+        
+        let mut count = 0;
+        for game in p.parse(file.unwrap()) {
+            chess_writer.write(&game).unwrap();
+            count += 1;
+        }
+        println!("{} games written in {:?}.", count, start.elapsed());
+    } else {
+        println!("{} games red in {:?}.", p.parse(file.unwrap()).count(), start.elapsed());
+    }
+}
+
+fn white_wins(tags: &HashMap<String,String>) -> bool {
+    tags.get("Result").map_or_else(|| false, |r| r == "1-0")
+}
+
+fn black_wins(tags: &HashMap<String,String>) -> bool {
+    tags.get("Result").map_or_else(|| false, |r| r == "0-1")
+}
+
+fn draw(tags: &HashMap<String,String>) -> bool {
+    tags.get("Result").map_or_else(|| false, |r| r == "1/2-1/2")
 }
