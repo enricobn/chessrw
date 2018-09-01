1551766cd94ba97061a34b17afd036b3b1543706
Enrico Benedetti <enrico_benedetti@yahoo.com>
2018-09-01 19:21:33 +0200

    Remove "0 seconds" from format_duration.

--- src/bin/chessrw.rs
+++ src/bin/chessrw.rs
@@ -113,10 +113,13 @@ pub fn main() -> std::io::Result<()> {
 }
 
 fn format_duration(duration: Duration) -> String {
-    if duration.as_secs() == 1 {
+    let secs = duration.as_secs();
+    if secs == 0 {
+        format!("{} millis", duration.subsec_millis().separated_string())
+    } else if secs == 1 {
         format!("1 second {} millis", duration.subsec_millis().separated_string())
     } else {
-        format!("{} seconds {} millis", duration.as_secs().separated_string(), duration.subsec_millis().separated_string())
+        format!("{} seconds {} millis", secs.separated_string(), duration.subsec_millis().separated_string())
     }
 }
 
